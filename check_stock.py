8b928a8ffec890186d6bba1869b8c1794f891b4d
isshii0063 <y1u2y2a7_1227@yahoo.co.jp>
2026-07-29 12:25:16 +0900

    Update check_stock.py

--- check_stock.py
+++ check_stock.py
@@ -1,1 +1,10 @@
-print("ポケカ監視テスト")
+import requests
+import os
+
+webhook = os.environ["DISCORD_WEBHOOK"]
+
+requests.post(webhook, json={
+    "content": "🎉 ポケカ監視Bot テスト成功！"
+})
+
+print("Discordへ送信しました")
